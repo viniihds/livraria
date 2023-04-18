ad298af9367897462f847a1780e2d7f2f365f5e7
viniihds <viniciushsilva2210@gmail.com>
2023-04-18 14:42:18 -0300

    Criação Serializer Editora

--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from livraria.models import Categoria
+from livraria.models import Categoria, Editora
+
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
+        fields = "__all__"
+
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
         fields = "__all__"
