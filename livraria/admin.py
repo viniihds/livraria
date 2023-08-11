4d1ff2a98b13ff964ecc886a637b0dcc61709fd1
viniihds <viniciushsilva2210@gmail.com>
2023-08-11 14:43:01 -0300

    Models Compra e ItensCompra

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Autor, Categoria, Editora, Livro
+from .models import Autor, Categoria, Editora, Livro, Compra, ItensCompra
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
 admin.site.register(Autor)
+admin.site.register(Compra)
+admin.site.register(ItensCompra)
---
+++ livraria/models/compra.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from usuario.models import Usuario
+from livraria.models import Livro
+
+class Compra(models.Model):
+    class StatusCompra(models.IntegerChoices):
+        CARRINHO = (1, "Carrinho")
+        REALIZADO = (2,"Realizado")
+        PAGO = (3, "Pago")
+        ENTREGUE = (4, "Entregue")
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name="compras")
+    status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+
+
+class ItensCompra(models.Model):
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
+    quantidade = models.IntegerField(default=1)
