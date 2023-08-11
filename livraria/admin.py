f1e36c0fbc2e87471461cdbae4f28ca1f0192698
viniihds <viniciushsilva2210@gmail.com>
2023-08-11 14:55:10 -0300

    Aula 26

--- livraria/admin.py
+++ livraria/admin.py
@@ -6,5 +6,11 @@ admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
 admin.site.register(Autor)
-admin.site.register(Compra)
-admin.site.register(ItensCompra)
+
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
