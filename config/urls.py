8b871998862896b52f0ee9df51802211a8ab3bf7
viniihds <viniciushsilva2210@gmail.com>
2023-09-01 14:33:28 -0300

    Serializer e View Compra

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, CompraViewSet
 from uploader.router import router as uploader_router
 from usuario.router import router as usuario_router
 
@@ -23,6 +23,7 @@ router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 router.register(r"autores", AutorViewSet)
 router.register(r"livros", LivroViewSet)
+router.register(r"compras", CompraViewSet)
 
 urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
